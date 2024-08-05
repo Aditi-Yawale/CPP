b0923f518756943125fef5f818dd87826cb20cd1
Aditi Yawale <yawaleaditi@gmail.com>
2024-08-06 00:18:08 +0530

    Add files via upload
    //Program to print "Jay Ganesh" on the screen, as a start to my journey!

---
+++ Jay_Ganesh.cpp
@@ -0,0 +1,12 @@
+//Program to print "Jay Ganesh" on the screen, as a start to my journey!
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
+
